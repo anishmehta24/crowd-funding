86c77f450a7f7431d6ba6949bf605649962ed3c5
Anish Mehta <mehtaanish111@gmail.com>
2025-03-27 17:56:18 +0530

    Add getDonations function to retrieve and format donation data for a specific campaign

--- Context/CrowdFunding.js
+++ Context/CrowdFunding.js
@@ -96,4 +96,21 @@ export const CrowdFundingProvider = ({children}) => {
         return campaignData;
 
     }
+
+    const getDonations = async(pId) => {
+        const provider = new ethers.providers.JsonRpcProvider();
+        const contract = fetchContract(provider);   
+
+        const donations = await contract.getDonators(pId);
+        const numberOfDonations = donations[0].length;
+        const parsedDonations = [];
+        for(let i=0;i<numberOfDonations;i++) {
+            parsedDonations.push({
+                donator:donations[0][i],
+                donation:ethers.utils.formatEther(donations[1][i].toString())
+            })
+        }
+
+        return parsedDonations
+    }
 }
